3c4f5d88af26a243076446bd3e3f4a44a7565030
Prasun Timalsina <timalsinaprasun61@gmail.com>
2025-03-01 17:33:38 +0545

    Complete accordion challenge

--- challenge-7/app.js
+++ challenge-7/app.js
@@ -1,3 +1,32 @@
 /**
  * Write your challenge solution here
  */
+
+//elements
+const accordionItemBtns = Array.from(
+  document.querySelectorAll(".accordion-button")
+);
+const accordionItems = Array.from(document.querySelectorAll(".accordion-item"));
+
+//functions
+const offAccordions = (exceptIndex) => {
+  accordionItems.forEach((accordionItem, index) => {
+    if (index !== exceptIndex) {
+      accordionItem.classList.remove("active");
+    }
+  });
+};
+
+const toggleAccordings = () => {};
+
+//events
+accordionItemBtns.forEach((accordionItemBtn, index) => {
+  accordionItemBtn.addEventListener("click", () => {
+    //open toggle  content of clicked accordion
+    const accordionItem = accordionItemBtn.parentElement;
+
+    //remove active of all accordions except the current one being clicked
+    offAccordions(accordionItems.indexOf(accordionItem));
+    accordionItem.classList.toggle("active");
+  });
+});
