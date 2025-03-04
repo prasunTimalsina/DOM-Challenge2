b6aa46175c0cc7aae15c13c010113eeb9380e45d
Prasun Timalsina <timalsinaprasun61@gmail.com>
2025-03-04 22:48:11 +0545

    Complete Memory Game

--- challenge-10/app.js
+++ challenge-10/app.js
@@ -1,12 +1,15 @@
 ///elements
 const gameContainer = document.querySelector(".game-container");
-
+const movesEl = document.getElementById("moves");
+const timeEl = document.getElementById("time");
+let timerId;
 //variables
 const state = {
   moves: 0,
   emojies: ["🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🐻", "🐼"],
   shuffledArray: [],
-  timer: null,
+  totalSeconds: 0,
+  currentSelectedEmojies: [],
 };
 
 //shuffling algo
@@ -29,7 +32,30 @@ const shuffleArray = () => {
   }
 };
 
+//timer function
+const initTimer = () => {
+  timerId = setInterval(() => {
+    state.totalSeconds++;
+
+    let formatedMin = String(Math.floor(state.totalSeconds / 60)).padStart(
+      2,
+      "0"
+    );
+    let formatedSecond = String(state.totalSeconds % 60).padStart(2, "0");
+
+    timeEl.textContent = `${formatedMin}:${formatedSecond}`;
+  }, 1000);
+};
+
+const clearTimer = () => {
+  clearInterval(timerId);
+  timeEl.textContent = "0:00";
+};
+
+//dom events
 const shuffleGrid = () => {
+  gameContainer.textContent = "";
+
   state.shuffledArray.forEach((emoji, index) => {
     const card = document.createElement("div");
     card.classList.add("card");
@@ -40,43 +66,96 @@ const shuffleGrid = () => {
     gameContainer.appendChild(card);
 
     card.addEventListener("click", () => {
-      card.classList.add("flipped");
-      insideCard.classList.remove("card-front");
-      insideCard.classList.add("card-back");
-      insideCard.textContent = state.shuffledArray[index];
+      flipCard(card, index);
+      if (state.currentSelectedEmojies.length < 2) {
+        // condition for user to prevent from double click
+        if (state.currentSelectedEmojies.length > 0) {
+          if (state.currentSelectedEmojies[0].index !== index) {
+            state.currentSelectedEmojies.push({ index, card });
+          }
+        } else {
+          state.currentSelectedEmojies.push({ index, card });
+        }
+      }
+      if (state.currentSelectedEmojies.length === 2) {
+        playMove(state.currentSelectedEmojies);
+      }
     });
   });
 };
 
+//UI related stuffs
+const flipCard = (target, indexOfEmoji) => {
+  target.classList.add("flipped");
+  const insideCard = target.querySelector(".card-front");
+  if (!insideCard) return;
+  insideCard.classList.remove("card-front");
+  insideCard.classList.add("card-back");
+  insideCard.textContent = state.shuffledArray[indexOfEmoji];
+};
+
+const removeFlip = (target) => {
+  target.classList.remove("flipped");
+  const insideCard = target.querySelector(".card-back");
+  insideCard.classList.remove("card-back");
+  insideCard.classList.add("card-front");
+  insideCard.textContent = "?";
+};
+
+const updateMove = () => {
+  state.moves++;
+  movesEl.textContent = state.moves;
+};
+
 //playMove
-const playMove = (index1, index2) => {
-  /* const index1 = prompt("Choose a index");
-  const index2 = prompt("Choose another index"); */
+const playMove = () => {
+  const cell1 = state.currentSelectedEmojies[0].card;
+  const cell2 = state.currentSelectedEmojies[1].card;
+
+  const index1 = state.currentSelectedEmojies[0].index;
+  const index2 = state.currentSelectedEmojies[1].index;
+
   const emoji1 = state.shuffledArray[index1];
   const emoji2 = state.shuffledArray[index2];
   if (emoji1 === emoji2) {
     console.log("matched");
     const index = state.emojies.indexOf(emoji1);
     state.emojies.splice(index, 1);
-    console.log(state);
-    state.moves++;
+
+    updateMove();
+    state.currentSelectedEmojies.length = 0;
     if (state.emojies.length <= 0) {
-      console.log("you won");
-    } else {
-      playMove();
+      setTimeout(() => {
+        alert("You won:🎉🎉: Click ok for New Game");
+        restartGame();
+      }, 500);
     }
   } else {
+    updateMove();
+    setTimeout(() => {
+      removeFlip(cell1);
+      removeFlip(cell2);
+    }, 500);
+    state.currentSelectedEmojies.length = 0;
     console.log("didn't matched");
-    playMove();
-    state.moves++;
   }
+};
+
+const restartGame = () => {
+  state.emojies = ["🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🐻", "🐼"];
+  state.totalSeconds = 0;
+  state.moves = 0;
+  state.currentSelectedEmojies = [];
+  state.shuffledArray = [];
+  movesEl.textContent = 0;
+  init();
+  clearTimer();
 };
 
 const init = () => {
   shuffleArray();
-  console.log(state.shuffledArray);
   shuffleGrid();
-  /* playMove(); */
+  initTimer();
 };
 
 init();
